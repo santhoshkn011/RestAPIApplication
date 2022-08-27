ee9ec97e77db3fe1853aaf50616450029b440bda
santhoshkn011 <santhoshkn011@gmail.com>
2022-08-27 11:06:10 +0530

    Added: Passing name as a parameter.

--- src/main/java/com/example/restapis/controller/RestAPI.java
+++ src/main/java/com/example/restapis/controller/RestAPI.java
@@ -1,9 +1,6 @@
 package com.example.restapis.controller;
 
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
@@ -12,8 +9,11 @@ public class RestAPI {
     public String welcome(){
         return "<h1>Hello From BridgeLabz!!</h1>";
     }
-    @RequestMapping(value = {"/greet/{name}"}, method = RequestMethod.GET)
-    public String greet(@PathVariable String name) {
-        return "Hello, " + name + " from BridgeLabz!!";
+
+    //Use GET Request Method and pass name as query parameter
+    @GetMapping("/query")
+    public String getUsers(@RequestParam(value = "name", defaultValue = "Santhosh") String name)
+    {
+        return "Hello! " + name + " From BridgeLabz!!";
     }
 }
