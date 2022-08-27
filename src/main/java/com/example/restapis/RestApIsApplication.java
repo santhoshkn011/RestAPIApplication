04fafdbec8b75ef923f4671684f5e5d6074228fa
santhoshkn011 <santhoshkn011@gmail.com>
2022-08-27 10:06:23 +0530

    Added: Rest API Spring boot application.

---
+++ src/main/java/com/example/restapis/RestApIsApplication.java
@@ -0,0 +1,13 @@
+package com.example.restapis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApIsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestApIsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/restapis/RestApIsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restapis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApIsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
